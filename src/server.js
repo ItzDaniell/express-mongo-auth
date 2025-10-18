934dc16d3ead5c81a2eb3b0a96c9e2e6e540e1ac
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-10-18 18:55:55 -0500

    chore: resolving an error

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
